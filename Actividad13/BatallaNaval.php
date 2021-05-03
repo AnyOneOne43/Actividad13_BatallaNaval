5092c03a135adee4c7e7a61460620a2c71a97991
Any <anybodyanyoneone@gmail.com>
2021-05-03 17:32:20 -0500

    Correción de tabla y creo que ya tengo lo de vidas

--- Actividad13/BatallaNaval.php
+++ Actividad13/BatallaNaval.php
@@ -1,17 +1,30 @@
 <?php
-    $vida=8;
+    if ($_POST['Enviar']=="Disparar!!")
+    {
+        
+        $vida=$_POST['vidad']; 
+    }
+    else {
+        
+        $vida=8;
+    }
+    for($i=0;$i<=10;$i++){
+        for($j=0;$j<=10;$j++){
+                $arreglo[$i][$j]="<img src='./Mar.png' width='30' height='30'></th>";
+          }
+      }
     echo "<h1>Batalla Naval</h1>";
     echo "<h3>Vidas:";
     //despliega las vidas
-        for($i=0;$i<=$vida;$i++)
+        for($i=1;$i<=$vida;$i++)
         {
             echo "<img src='./corazon.png' width='20'>";
         }
     echo "</h3>";
     echo "Historial de Disparos:";
     echo "<br><br>";
-    echo "<table border=1>";
 
+    echo "<table border=1>";
       echo "<tr>";
         echo "<th> </th>";
         echo "<th>A</th>";
@@ -26,19 +39,23 @@
         echo "<th>J</th>";
       echo "</tr>";
       //Hace el resto de la tabla
-      for($i=0;$i<=10;$i++){
+      for($i=1;$i<=10;$i++){
         echo "<tr>";
         echo "<th>$i</th>";
-        for($j=0;$j<=10;$j++){
-                echo "<th><img src='./Mar.png' width='30' height='30'></th>";
+        for($j=1;$j<=10;$j++){
+            echo "<th>";
+                echo $arreglo[$i][$j];
+            echo "</th>";
           }
         echo "</tr>";
       }
-    echo "</table";
+    echo "</table>";
+    echo "<br>";
 
     echo "<form action='BatallaNaval.php' method='post'>";
       echo "Posición X(Letra):<input type='text' name='Letra'>";
       echo "Posición Y(Número):<input type='text' name='Num'>";
+      echo "<input type='hidden' name='vidad' value=$vida>";
       echo "<input type='submit' name='Enviar' value='Disparar!!'>";
     echo "</form>";
     echo "<br><br>";
